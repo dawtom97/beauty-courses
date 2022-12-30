445ab8309c1c0b6f089075d9ea154997386ecda2
Dawid <daw.tomczyk1997@gmail.com>
2022-12-30 12:44:38 +0100

    mail bugfix

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -5,31 +5,36 @@ require('dotenv').config();
 export default (req: any, res: any) => {
   let nodemailer = require('nodemailer');
   const transporter = nodemailer.createTransport({
-    //   port: 465,
-    //   host: 'mail31.mydevil.net',
-    //   auth: {
-    //       user: 'd.tomczyk@ernabo.pl',
-    //       pass: 'Titanum97',
-    //   },
-    //   secure: true,
-    port: 465,
-    host: 'smtp.gmail.com',
-    auth: {
-        user: 'daw.tomczyk1997@gmail.com',
-        pass: 'kfuubtckhttmtluv',
-    },
-    secure: true,
+      port: 465,
+      host: 'mail31.mydevil.net',
+      auth: {
+          user: 'd.tomczyk@ernabo.pl',
+          pass: 'Titanum97',
+      },
+      secure: true,
+    // port: 465,
+    // host: 'smtp.gmail.com',
+    // auth: {
+    //     user: 'ernaboszkolenia1@gmail.com',
+    //     pass: '12q1290nowe',
+    // },
+    // secure: true,
   })
 
   const mailData = {
-    from: req.body.email,
-    to: 'daw.tomczyk1997@gmail.com',
-    subject: `Wiadomość od ${req.body.firstname} ${req.body.lastname}`,
+    from: 'szkolenia.beauty@ernabo.pl',
+    to: 'd.tomczyk@ernabo.pl',
+    subject: `Nowe zgłoszenie na kurs: ${req.body.firstname} ${req.body.lastname}`,
     text: 'aaaaaa',
     html: `<div>
-    Numer telefonu: ${req.body.phone}
-    ${req.body.content}</div>
-    <p>Od:${req.body.email}</p>`,
+    <h3>${req.body.firstname} ${req.body.lastname}</h3>
+    <p>Numer telefonu: <strong> ${req.body.phone} </strong></p>
+    <p>Email: <strong> ${req.body.email} </strong></p>
+    <br>
+    <hr>
+    ${req.body.content}
+    </div>
+   `,
   };
   transporter.sendMail(mailData, function (err: any, info: any) {
     if (err) console.log(err);
